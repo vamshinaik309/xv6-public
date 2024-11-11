715259dbe2c82819899969d9a0be1f1ee80a2d72
vamshinaik309 <46342370+vamshinaik309@users.noreply.github.com>
2024-11-11 07:26:25 +0000

    added pipe in primeproc

--- primeproc.c
+++ primeproc.c
@@ -3,27 +3,17 @@
 #include "stat.h"
 #include "user.h"
 
-int mutexid; 
-
-void 
-forloop(int num)
-{
-    int x =0;
-    for(int z = 0; z < 3000000000; z+=1)
-	    x = x + 3.14*89.64;
-}
+int mutexid;
 
 void wait_for_seconds(int seconds) {
-    int start_ticks = uptime();  
+    int start_ticks = uptime();
     while (uptime() - start_ticks < seconds * 100) {
     }
 }
 
-int
-is_prime(int num)
-{
+int is_prime(int num) {
     if (num <= 1) return 0;
-    for(int i = 2; i <= num / 2; i++){
+    for(int i = 2; i <= num / 2; i++) {
         if(num % i == 0){
             return 0;
         }
@@ -31,33 +21,32 @@ is_prime(int num)
     return 1;
 }
 
-void
-calculate_primes(int limit, int mutexid)
-{
+void calculate_primes(int limit, int mutexid) {
     int num = 2;
-    int count = 0; 
+    int count = 0;
 
     while(count < limit) {
         if(is_prime(num)) {
             lock(mutexid);
             printf(1, "PID %d printing %d th Prime number:%d\n", getpid(), count + 1, num);
-            unlock(mutexid); 
+            unlock(mutexid);
 
             count++;
-            wait_for_seconds(1); 
-            // sleep(1);
+            wait_for_seconds(1);
         }
-        num++; 
+        num++;
     }
 }
 
-int
-main(int argc, char *argv[])
-{
-    int n = 1; 
+int main(int argc, char *argv[]) {
+    int n = 1;
+    int pipe_fd = -1;
 
     if(argc > 1)
         n = atoi(argv[1]);
+
+    if (argc > 2)
+        pipe_fd = atoi(argv[2]); // Get the write end of the pipe from the arguments
 
     if (n < 1)
         n = 1;
@@ -77,12 +66,18 @@ main(int argc, char *argv[])
             exit();
         } else if(pid == 0) {
             printf(1, "Child %d started\n", getpid());
-            calculate_primes(1000, mutexid); 
+            calculate_primes(1000, mutexid);
             printf(1, "Child %d finished\n", getpid());
             exit();
         } else {
             printf(1, "Parent %d created child %d\n", getpid(), pid);
         }
+    }
+
+    // Signal that all child processes have been created
+    if(pipe_fd >= 0) {
+        write(pipe_fd, "done", 4);
+        close(pipe_fd); // Close the write end after signaling
     }
 
     for(k = 0; k < n; k++) {
--- task2_test3.c
+++ task2_test3.c
@@ -3,34 +3,98 @@
 #include "stat.h"
 #include "user.h"
 
-#define TICKS_PER_SECOND 100 // Adjust according to xv6 configuration
+void itoa(int num, char *str) {
+    int i = 0;
+    if (num == 0) {
+        str[i++] = '0';
+    } else {
+        // Handle each digit and convert to string in reverse order
+        int temp = num;
+        while (temp != 0) {
+            str[i++] = (temp % 10) + '0';
+            temp /= 10;
+        }
+    }
+    str[i] = '\0';
+
+    // Reverse the string
+    for (int j = 0; j < i / 2; j++) {
+        char swap = str[j];
+        str[j] = str[i - j - 1];
+        str[i - j - 1] = swap;
+    }
+}
 
 int main() {
-    // Step 1: Run 'primeproc 3 &' in the background
+    int pid;
+    int pipe_fd[2];
+
+    if (pipe(pipe_fd) < 0) {
+        printf(1, "Error: Unable to create pipe\n");
+        exit();
+    }
+
+    // Run 'primeproc 3 &' in the background
     printf(1, "Running 'primeproc 3 &' in the background\n");
-    if (fork() == 0) { // Child process to execute primeproc
-        char *args[] = {"primeproc", "3", 0};
+    pid = fork();
+    if (pid == 0) { // Child process to execute primeproc
+        close(pipe_fd[0]); // Close read end in child
+
+        // Convert write end file descriptor to string
+        char fd_str[10];
+        itoa(pipe_fd[1], fd_str);
+
+        char *args[] = {"primeproc", "3", fd_str, 0};
         exec("primeproc", args);
         printf(1, "Error: exec primeproc failed\n");
         exit();
-    }
+    } else if (pid > 0) {
+        close(pipe_fd[1]); // Close write end in parent
 
-    // Step 2: Busy-wait loop to ensure primeproc has started and its child processes are running
-    int start_ticks = uptime();
-    while (uptime() - start_ticks < 2 * TICKS_PER_SECOND); // Wait for approximately 2 seconds
+        // Wait for the signal from primeproc
+        char buf[5];
+        int n = read(pipe_fd[0], buf, 4);
+        if (n != 4) {
+            printf(1, "Error: Did not receive proper signal from primeproc\n");
+            exit();
+        }
+        buf[4] = '\0';
+        if (strcmp(buf, "done") == 0) {
+            printf(1, "\nAll children of primeproc are created and running.\n");
+        } else {
+            printf(1, "Error: Received unexpected signal from primeproc: %s\n", buf);
+            exit();
+        }
+        close(pipe_fd[0]); // Close read end
 
-    // Step 3: Run 'nice 7 3' to set priority for PID 3
-    printf(1, "\nRunning 'nice 7 3'\n");
-    if (fork() == 0) { // Child process to execute nice
-        char *args[] = {"nice", "7", "3", 0};
-        exec("nice", args);
-        printf(1, "Error: exec nice failed\n");
+        // Run 'nice 7 <pid>' where <pid> is the PID of primeproc
+        char pid_str[10];
+        itoa(pid, pid_str);
+        printf(1, "\nRunning 'nice 7 %s'\n", pid_str);
+        if (fork() == 0) { // Child process to execute nice
+            char *args[] = {"nice", "7", "3", 0};
+            exec("nice", args);
+            printf(1, "Error: exec nice failed\n");
+            exit();
+        }
+        wait(); // Wait for nice to complete
+
+        // Optionally, run 'ps' to display process statuses
+        printf(1, "\nRunning 'ps' to display process statuses\n");
+        if (fork() == 0) { // Child process to execute ps
+            char *args[] = {"ps", 0};
+            exec("ps", args);
+            printf(1, "Error: exec ps failed\n");
+            exit();
+        }
+        wait(); // Wait for ps to complete
+
+        // Wait for primeproc to finish
+        wait();
+    } else {
+        printf(1, "Error: fork failed\n");
         exit();
     }
-
-    // Step 4: Wait for child processes to finish
-    wait(); // Wait for the first fork (primeproc)
-    wait(); // Wait for the second fork (nice)
 
     exit();
 }
